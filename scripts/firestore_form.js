eaa458e61738c1b8154a244ceb96be8f51eb40c9
RubyZulu <cyber4humans@gmail.com>
2022-10-10 19:04:04 +0100

    Update firestore_form.js

--- scripts/firestore_form.js
+++ scripts/firestore_form.js
@@ -1,24 +1,36 @@
 // Initialize Firebase
 // your firebase info for your project should go here.
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDfvBEmPsp2RhOHQ4cmlr-3_kVwW2mVFOI",
-  authDomain: "gwse1-d1302.firebaseapp.com",
-  projectId: "gwse1-d1302",
-  storageBucket: "gwse1-d1302.appspot.com",
-  messagingSenderId: "458621958114",
-  appId: "1:458621958114:web:f1f3748aaa8a0fc4482bbb",
-  measurementId: "G-PQ567NHR2T"
+  apiKey: "AIzaSyAAv2f43xu6x-odfXpjxXQQbvXGdG6b6P0",
+  authDomain: "gwse-79990.firebaseapp.com",
+  databaseURL: "https://gwse-79990-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "gwse-79990",
+  storageBucket: "gwse-79990.appspot.com",
+  messagingSenderId: "191714628545",
+  appId: "1:191714628545:web:e521a44d0f5ea4df1b5540"
 };
+
+// Initialize Firebase
+
+
+// copied from google to here
 
 const saveButton = document.querySelector("#saveButton");
 
 saveButton.addEventListener("click", function() {
 	alert(1);
-	
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);	
+//firebase.initializeApp(firebaseConfig);
 
-const firestore = firebase.firestore();
-const docRef = firestore.doc("applications/person");
+//const firestore = firebase.firestore();
+//const docRef = firestore.doc("applications/person");
 
 
 const firstName = document.getElementById("firstName");
@@ -31,6 +43,17 @@ alert(lastName);
     const firstNameValue = firstName.value;alert(2);alert(firstNameValue);
 
     const lastNameValue = lastName.value;alert(3);alert(lastNameValue);
+	
+    db.collection("person").doc("p").set({
+    fname: firstNameValue,
+    lname: lastNameValue,
+})
+.then(() => {
+    console.log("Document successfully written!");
+})
+.catch((error) => {
+    console.error("Error writing document: ", error);
+});
     
     writeNewApp(firstNameValue,lastNameValue);
     alert("Thank you, your application has been saved");
