a67a038998602d8310d36d146b1a58f7fb511f53
RubyZulu <cyber4humans@gmail.com>
2022-10-10 19:30:52 +0100

    Update firestore_form.js

--- scripts/firestore_form.js
+++ scripts/firestore_form.js
@@ -16,83 +16,28 @@ const firebaseConfig = {
   appId: "1:191714628545:web:e521a44d0f5ea4df1b5540"
 };
 
-// Initialize Firebase
+saveButton.addEventListener("click", function() {
+
+firebase.initializeApp(firebaseConfig);
+
+const firestore = firebase.firestore();
+const docRef = firestore.doc("users/person");
 
 
-// copied from google to here
-
+const firstName = document.querySelector("#firstName");
+const lastName = document.querySelector("#lastName");
 const saveButton = document.querySelector("#saveButton");
 
-saveButton.addEventListener("click", function() {
-	
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);	
-//firebase.initializeApp(firebaseConfig);
-
-//const firestore = firebase.firestore();
-//const docRef = firestore.doc("applications/person");
-
-
-const firstName = document.getElementById("firstName");
-const lastName = document.getElementById("lastName");
-//
-
-
-
-    const firstNameValue = firstName.value;alert(firstNameValue);
-    const lastNameValue = lastName.value;alert(lastNameValue);
-	
-    
-    	var docData = {
-    stringExample: "Hello world!",
-    booleanExample: true,
-    numberExample: 3.14159265,
-    dateExample: firebase.firestore.Timestamp.fromDate(new Date("December 10, 1815")),
-    arrayExample: [5, true, "hello"],
-    nullExample: null,
-    objectExample: {
-        a: 5,
-        b: {
-            nested: "foo"
-        }
-    }
-};
-db.collection("data").doc("one").set(docData).then(() => {
-    console.log("Document successfully written!");
+    const firstNameValue = firstName.value;
+  
+    const lastNameValue = lastName.value;
+   
+    docRef.set({
+        firstName: firstNameValue,
+        lastName: lastNameValue,
+    }).then(function() {
+        console.log("Status saved!");
+    }).catch(function (error) {
+        console.log("Got an error: ", error);
+    })
 });
-    //writeNewApp(firstNameValue,lastNameValue);
-    //alert("Thank you, your application has been saved");
-
-   // docRef.set({
-   //     firstName: firstNameValue,
-   //     lastName: lastNameValue,
-   // }).then(function() {
-   //     console.log("Status saved!");
-   // }).catch(function (error) {
-   //     console.log("Got an error: ", error);
-   // })
-});
-
-
-
-//function writeNewApp(firstname, lastname) {
-  // A post entry.
-
-	//return;
-	
-	
-//  var postData = {
-  //  firstname: firstname,
- //   lastname: lastname
- // };
-
-  // Get a key for a new Post.
-  //var newPostKey = firebase.db().ref().child('applications/person').push().key;
-
-  // Write the new post's data simultaneously in the posts list and the user's post list.
-  //var updates = {};
-  //updates['/applications/' + newPostKey] = postData;
-  //updates['/user-posts/' + uid + '/' + newPostKey] = postData;
-
-  //return firebase.db().ref().update(updates);
-//}
